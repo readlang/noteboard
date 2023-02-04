48c538ca1b12dddaf94e0d2df39a47dc6d658e0f
Read Langworthy <rlangworthy@gmail.com>
2023-02-03 16:10:31 -0800

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,127 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import styled from "styled-components";
+import NoteCard from "./NoteCard";
+
+const headerHeight = 50;
+
+const HeaderBar = styled.div`
+  width: 100%;
+  height: ${headerHeight}px;
+  background-color: rgb(47, 255, 0);
+`
+const HeaderBarContent = styled.div`
+  padding: 10px 40px;
+  color: rgb(36, 36, 36);
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`
+const Canvas = styled.div`
+  position: relative;
+  height: calc(100vh - ${headerHeight}px);
+  background-color: hsl(0, 0%, 98%);
+`
+const AddButton = styled.button`
+  position: absolute;
+  left: 40px;
+  top: 40px;
+  height: 38px;
+  width: 38px;
+  border-radius: 40px;
+  background-color: hsla(0, 0%, 100%, 1);
+  border: 2px solid rgb(0 122 255);
+  color: rgb(0 122 255);
+`
+const SaveButton = styled.button`
+  height: 20px;
+  width: auto;
+  border-radius: 5px;
+  background-color: hsla(0, 0%, 100%, 1);
+  border: 1px solid rgb(0 122 255);
+  color: rgb(0 122 255);
+`
 
 function App() {
+
+  const exampleNote = {
+    id: 1,
+    title: "Example Note",
+    text: "This is the text area.",
+    posX: 300,
+    posY: 300,
+  }
+
+  const [notes, setNotes] = useState([exampleNote])
+
+  function addNote() {
+    const newId = notes[notes.length-1].id+1
+    const newNote = {
+      id: newId,
+      title: `New note ${newId}`,
+      text: "This is the text area. This is the text area.",
+      posX: 100,
+      posY: 100,
+    } 
+    setNotes([...notes, newNote])
+  }
+
+  function editNote(edittedNote) {
+    let copyNotes = [...notes]
+    copyNotes[copyNotes.findIndex(item => item.id === edittedNote.id)] = edittedNote
+    setNotes(copyNotes)
+  }
+
+  // function deleteNote() {
+    
+  // }
+
+  function saveToLocal() {
+    localStorage.setItem('NBnotes', JSON.stringify(notes) )
+  }
+
+  function getFromLocal() {
+    const jsonData = localStorage.getItem('NBnotes')
+
+    if (jsonData) {
+      try {
+        let retrievedData = JSON.parse(jsonData)
+        setNotes(retrievedData)
+      } catch (error) {
+        console.log("There was a error retrieving the data.  Error shown below.  Use 'save to local' to overwrite corrupt data.")
+        console.error("NoteBoard error: Data saved to local is corrupt -", error)
+      }
+    } else {
+      console.log("No saved data to retrieve.")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <HeaderBar>
+        <HeaderBarContent>
+          <h2>N o t e B o a r d</h2>
+          <div>
+            <SaveButton onClick={saveToLocal}>Save to local</SaveButton> &nbsp;
+            <SaveButton onClick={getFromLocal}>Load from local</SaveButton>
+          </div>
+        </HeaderBarContent>
+      </HeaderBar>
+
+      <Canvas>
+        <AddButton onClick={addNote}><h1>+</h1></AddButton>
+        {notes.map( (note, index) => 
+          <NoteCard key={`${index}-${note.title}`} note={note} editNote={editNote}/> 
+        )}
+
+        <br/> <br/> <br/> <br/> <br/> <br/> <br/> 
+        <h1>hello</h1>
+        <h2>hello</h2>
+        <h3>hello</h3>
+        <h4>hello</h4>
+        <h5>hello</h5>
+        <h6>hello</h6>
+        
+      </Canvas>
     </div>
   );
 }
---
+++ src/NoteCard.js
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+    position: absolute;
+    left: ${props => props.x || "100"}px;
+    top: ${props => props.y || "100"}px;
+    width: 200px;
+    height: 200px;
+`
+const Header = styled.div`
+    width: 100%;
+    height: 20px;
+    background-color: rgb(47, 255, 0);
+    border-radius: 10px 10px 0 0;
+`
+const Body = styled.div`
+    width: calc(100% - 22px);
+    padding: 10px;
+    border: 1px solid rgb(47, 255, 0);
+    border-radius: 0 0 10px 10px;
+`
+
+function NoteCard({note, editNote}) {
+    const [startPos, setStartPos] = useState()
+
+    function moveNote(finalX, finalY) {
+        let copyNote = {...note}
+        copyNote.posX = note.posX + finalX - startPos.x
+        copyNote.posY = note.posY + finalY - startPos.y
+        editNote(copyNote)
+    }
+
+    return(
+        <Container x={note.posX} y={note.posY} draggable = "true"
+            onDragStart={(e) => setStartPos({x: e.clientX, y: e.clientY})} 
+            onDragEnd={(e) => moveNote(e.clientX, e.clientY) } >
+            <Header />
+            <Body>
+                <h3>{note.title} </h3>
+                <p>{note.text} </p>
+            </Body>
+        </Container>
+    )
+}
+
+export default NoteCard;
